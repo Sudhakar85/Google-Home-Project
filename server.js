e17e11da373d6be6a4b54ae0dec908a9600a3000
Sudhakar Annamalai <a.sudhakar85@gmail.com>
2017-12-25 01:04:24 -0600

    Update server.js

--- server.js
+++ server.js
@@ -11,3 +11,6 @@ var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
     
 app.listen(port, ip);
+
+console.log('Server running on http://%s:%s', ip, port);
+
