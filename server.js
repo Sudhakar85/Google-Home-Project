792c622de5e35646e15c6cc07501fe86b0edde06
Sudhakar Annamalai <a.sudhakar85@gmail.com>
2017-12-25 01:03:40 -0600

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 var express = require('express');
 var app = express();
+
 app.get('/', function (req, res) {
 
   res.send('Hello World!');
   
 });
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!');
-});
+var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
+    ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0',
+    
+app.listen(port, ip);
